a474cedc53a7cc13fbdb06e50470459214216b16
Johan van der Wijk <johan@vanderwijk.nl>
2020-03-03 09:54:58 +0100

    Wide images

--- taxonomy-dossier.php
+++ taxonomy-dossier.php
@@ -11,11 +11,30 @@
 	</div>
 </div>
 
-<div class="row content">
+<div class="row">
 	<?php
 		if ( have_posts() ) :
 			while ( have_posts() ) : the_post();
-				get_template_part( 'content', get_post_type() );
+
+					echo '<div class="wide flex"><div class="thumbnail"><a href="' . get_the_permalink() . '">';
+					$image = wp_get_attachment_image_src( get_post_thumbnail_id( $post ->ID ), 'single' );
+					echo '<img src="' . $image[0] . '" alt="">';
+					echo '</a></div><div class="description"><a href="' . get_the_permalink() . '"><h3>' . get_the_title() . '</h3>';
+					the_excerpt();
+					echo '</a>';
+					echo '<p><a href="' . get_the_permalink() . '" class="read-more">';
+					$post_type = get_post_type( get_the_ID() );
+					if ( $post_type == 'post' ) {
+						echo 'lees artikel';
+					} else if ( $post_type == 'project' ) {
+						echo 'bekijk project';
+					} else {
+						echo 'lees meer';
+					}
+					echo '</a></p>';
+					
+					echo '</div></div>';
+
 			endwhile;
 		else :
 			get_template_part( 'content', 'none' );
