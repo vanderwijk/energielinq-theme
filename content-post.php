812947a93e060158b92a5d109a750163bcd37cb1
Johan van der Wijk <johan@vanderwijk.nl>
2020-03-20 16:41:33 +0100

    The time

--- content-post.php
+++ content-post.php
@@ -13,7 +13,7 @@
 					<?php }; ?>
 
 					<div class="entry-meta">
-						<span class="date updated"><?php the_modified_date( get_option( 'date_format' ) ); ?></span>
+						<span class="date updated"><?php the_time( get_option( 'date_format' ) ); ?></span>
 						<span class="vcard author">
 							<?php _e( 'by', 'fran' ); ?>
 							<span class="fn"><?php the_author(); ?></span>
@@ -48,7 +48,7 @@
 					<h1 class="entry-title"><?php the_title(); ?></h1>
 
 					<div class="entry-meta">
-						<span class="date updated"><?php the_modified_date( get_option( 'date_format' ) ); ?></span>
+						<span class="date updated"><?php the_time( get_option( 'date_format' ) ); ?></span>
 						<span class="vcard author">
 							<?php _e( 'by', 'fran' ); ?>
 							<span class="fn"><?php the_author(); ?></span>
