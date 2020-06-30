ddbbed84bcdaae16f23989dd03d1befa09a27734
Johan van der Wijk <johan@vanderwijk.nl>
2020-06-30 17:45:06 +0200

    Gerelateerd verwijderd

--- content-project.php
+++ content-project.php
@@ -243,38 +243,4 @@
 				</div>
 			</div>
 
-			<?php 
-			$searchwp_related = new SearchWP_Related();
-
-			// Use the keywords as defined in the SearchWP Related meta box
-			$keywords = get_post_meta( get_the_ID(), $searchwp_related->meta_key, true );
-
-			$args = array(
-			's'              => $keywords,  // The stored keywords to use
-			'engine'         => 'projecten',  // the SearchWP engine to use
-			'posts_per_page' => 3,          // how many entries to find
-			);
-
-			// Retrieve Related content for the current post
-			$related_content = $searchwp_related->get( $args );
-
-			if ( $related_content ) { ?>
-
-			<div class="col">
-				<div class="block related">
-					<h3><?php _e( 'Related projects', 'energielinq' ); ?></h3>
-					<ul>
-						<?php
-							foreach ( $related_content as $related ) { ?>
-								<li><a href="<?php echo get_permalink( $related); ?>"><?php echo get_the_title( $related ); ?></a></li>
-							<?php
-							}
-						?>
-					</ul>
-				</div>
-			</div>
-
-			<?php wp_reset_postdata();
-			} ?>
-
 <?php } ?>
